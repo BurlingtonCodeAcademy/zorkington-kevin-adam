3b67d2e736798102d07dff30bf441a24aad8eb63
kevinmahoney95 <kevinmahoney95@gmail.com>
2021-02-03 16:31:30 -0500

    Changed welcome message

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ function ask(questionText) {
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+  const welcomeMessage = `You awake on a beach with no recollection of how you got there.
+   The sun beats down as the waves lap at your feet. To your immediate north, a boat rocks in the shallows. 
+   To the south, the beach stretches on as far the eye can see.`;
   let answer = await ask(welcomeMessage);
   console.log('Now write your code to make this work!');
   process.exit();
