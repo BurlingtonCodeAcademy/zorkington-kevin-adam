5bd50b4ccf27ec81cd94c24741047d3959d394fb
Adam Viens <adam_v69@yahoo.com>
2021-02-05 15:08:52 -0500

    change devs

--- index.js
+++ index.js
@@ -11,22 +11,6 @@ function ask(questionText) {
 }
 start();
 async function start() {
-  const welcomeMessage = `You wake up on a small fishing boat that has landed upon a beach. You have no recollection of how you got there. 
-  Cliffs rise a hundred feet from the sand in front of you to the east. To the north and west there is only open ocean. 
-  The beach stretches to the south as far as the eye can see. There are keys in the ignition...\n>_`;
-
-  let answer = await ask(welcomeMessage);
-
-  while (answer !== player.includes.this.action)
-    answer = await ask("I don't know how to " + answer + "?");
-
-  answer = await ask("beach1 description");
-
-  console.log("Now write your code to make this work!");
-
-  while (answer !== "exit" || answer !== "some text to move story along")
-    answer = await ask("I don't know how to " + answer + "?");
-}
 
 const player = {
   direction: ["north", "south", "east", "west", "up", "down", "left", "right"],
@@ -77,7 +61,7 @@ let torch = new Item(
   "torch",
   "A torch in a sconce on the cave wall. It partially lights the room.",
   true,
-  ["take", "drop", "throw", "pickup", "inspect", "examine", "look at"]
+  ["take", "drop", "throw", "pick up", "inspect", "examine", "look at"]
 );
 
 let skeleton = new Item(
@@ -87,24 +71,26 @@ let skeleton = new Item(
   ["inspect", "examine"]
 );
 
-let lighthouseKey = new Item("key", "An old rusted key.", true);
+let lighthouseKey = new Item("key", "An old rusted key.", true, ["take", "pick up", "drop", "inspect", "examine", "look at"]);
 
 let letter = new Item(
   "letter",
   "The letter reads as so: 'My darling, \n I fear this is the last letter I will ever write you. By my count, my relief should have arrived 4 months ago. \n I have been abandoned. I finished the last of my rations weeks ago, and the gulls no longer land close enough for me to catch.  \n I gaze into the lighthouse’s great beacon at night as it signals to an empty sea. It speaks to me. I dare not relay what it says. \n The sun mocks me. I can no longer stand its great heat or rays. I will find somewhere dark to rest.'",
-  true
+  true,
+  ["take", "pick up", "read", "inspect", "examine", "look at"]
 );
 
 let gasCan = new Item(
   "gas can",
   "An old rusted can of gas. It should be light enough to carry.",
-  true
+  true,
+  ["take", "pick up", "drop", "examine", "inspect", "look at"]
 );
 
 let boat = new Room(
   "boat",
   "An old fishing boat, worn but in working condition. There are keys in the ignition. It feels familiar, but you are sure you have never been here before.",
-  [boatKey],
+  ["boatKey"],
   ["beach1"]
 );
 
@@ -141,12 +127,12 @@ let cave1 = new Room(
 let cave2 = new Room(
   "cave2",
   "The light of your torch reveals a figure slumped against the far wall of the chamber. It is a skeleton. Only bones and tattered clothes remain."[
-    (lighthouseKey, letter, skeleton)
+    ("lighthouseKey", "letter", "skeleton")
   ],
   ["cave1"]
 );
 
-room = "boat";
+let currentRoom = "boat";
 
 let transitions = {
   boat: ["beach1"],
@@ -156,17 +142,49 @@ let transitions = {
   cave1: ["beach2", "cave2"],
   cave2: ["cave1"],
 };
+function string_to_array(str){
+  return str.trim().split(" ");
+}
 
 function changeRoom(nextRoom) {
-  if (transitions[room].includes(nextRoom)) {
-    room = nextRoom;
+  if (transitions[currentRoom].includes(nextRoom)) {
+    currentRoom = nextRoom;
   } else {
     console.log(`You can\'t get there from here`);
   }
 }
 function changeInv(item) {
   if (player.this.action === "take" || player.this.action === "pickup") {
-    currentRoom.inventory.pop()(player.playerInv.push(item));
+    currentRoom.inventory.pop()
+    (player.playerInv.push(item));
   } else if (player.this.action === "drop" || player.this.action === "throw")
     player.pop.this.playerInv && currentRoom.push.this.inventory;
+}
+const welcomeMessage = `You wake up on a small fishing boat that has landed upon a beach. You have no recollection of how you got there. 
+  Cliffs rise a hundred feet from the sand in front of you to the east. To the north and west there is only open ocean. 
+  The beach stretches to the south as far as the eye can see. There are keys in the ignition...\n>_`;
+
+  let answer = await ask(welcomeMessage);
+  string_to_array(answer);
+  let userIn = answer[0];
+
+
+
+  if (userIn[0] === player.movement.includes["move", "walk", "run", "climb", "head", "jump", "go"] && userIn[1] === player.direction.includes["south"]){
+    changeRoom(beach1)}
+    console.log(currentRoom)
+
+    //new room
+
+  //answer = await ask ("djfnvsijfn" + room.this.description + ".")
+  //string_to_array(answer)
+
+  
+
+  
+
+
+  //while answer !== "exit" || answer !== "some text to move story along"
+    //answer = await ask"I don't know how to " + answer + "?";
+
 }
